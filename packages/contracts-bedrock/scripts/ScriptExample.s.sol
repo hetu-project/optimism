4839db3824e6a9c1cce27e61ee764a15afcc2448
protolambda <proto@protolambda.com>
2024-08-13 11:23:09 -0600

    op-chain-ops: forge script cheatcodes, work in progress
    incl prank cheatcodes
    incl env var handling
    incl serialize utils
    incl some json/toml utils

--- packages/contracts-bedrock/scripts/ScriptExample.s.sol
+++ packages/contracts-bedrock/scripts/ScriptExample.s.sol
@@ -7,13 +7,33 @@ import { console2 as console } from "forge-std/console2.sol";
 /// @title ScriptExample
 /// @notice ScriptExample is an example script. The Go forge script code tests that it can run this.
 contract ScriptExample is CommonBase {
-
     /// @notice example function, runs through basic cheat-codes and console logs.
-    function run() public view {
+    function run() public {
+        bool x = vm.envOr("EXAMPLE_BOOL", false);
+        console.log("bool value from env", x);
+
         console.log("contract addr", address(this));
         console.log("contract nonce", vm.getNonce(address(this)));
         console.log("sender addr", address(msg.sender));
         console.log("sender nonce", vm.getNonce(address(msg.sender)));
+
+        string memory json = '{"root_key": {"hello": {"a": 1, "b": 2}}}';
+        string[] memory keys = vm.parseJsonKeys(json, ".root_key.hello");
+        console.log("keys", keys[0], keys[1]);
+
+        this.hello("from original");
+        vm.startPrank(address(uint160(0x42)));
+        this.hello("from prank 1");
+        console.log("parent scope", address(msg.sender), address(this));
+        this.hello("from prank 2");
+        vm.stopPrank();
+        this.hello("from original again");
+
         console.log("done!");
+    }
+
+    /// @notice example external function, to force a CALL, and test vm.startPrank with.
+    function hello(string calldata _v) external view {
+        console.log("hello addr", _v, address(msg.sender));
     }
 }
